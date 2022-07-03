e952ca134cfa0b4d5649d20257cce466b2406c2f
Your Name <49571769+Abdelwahabmostafa1996@users.noreply.github.com>
2022-07-03 08:51:43 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-footer dark color="blue">
+    <v-card flat tile class="blue white--text text-center">
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+          <v-icon size="30px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
+      <v-card-text class="white--text pt-0">
+        Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin.
+        Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque
+        penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} 
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
+};
+</script>
+
+<style></style>
